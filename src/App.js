af075e4de6ce46e5b70ab76f324354d8953f0ac2
Tommy47x <cristianleulbdt@gmail.com>
2023-10-18 22:54:54 +0300

    Fixed a bug related to the color of the alert-succes / danger

--- src/App.js
+++ src/App.js
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import Save from './Save';
 import './App.css';
 
-function App() {  // Component App is defined as a function that returns JSX code (HTML-like code)
-  const [savedWords, setSavedWords] = useState([]); // The state is initialized with an empty array
-  const [verificationText, setVerificationText] = useState(''); // The state is initialized with an empty string
-  const [message, setMessage] = useState(''); // The state is initialized with an empty string
 
-  const handleVerificationChange = (event) => { // This function is called every time the input field changes
-    setVerificationText(event.target.value); // The state is updated with the value of the input field
+function App() {
+  const [savedWords, setSavedWords] = useState([]);
+  const [verificationText, setVerificationText] = useState('');
+  const [message, setMessage] = useState('');
+  const [showMessage, setShowMessage] = useState(false); // Add a new state variable to track whether to show the message
+
+  const handleVerificationChange = (event) => {
+    setVerificationText(event.target.value);
+    setShowMessage(false); // Reset the showMessage state variable when the input field changes
   };
 
-  const handleVerificationSubmit = (event) => { // This function is called when the form is submitted
-    event.preventDefault(); // The default behavior of the form is prevented
-    if (savedWords.includes(verificationText)) { // If the word is in the dictionary
-      setMessage('The word is in the dictionary!'); // The message is updated
-    } else { // If the word is not in the dictionary
-      setMessage('The word is not in the dictionary.'); // The message is updated
+  const handleVerificationSubmit = (event) => {
+    event.preventDefault();
+    setShowMessage(true); // Set the showMessage state variable to true when the Verify button is pressed
+    if (savedWords.includes(verificationText)) {
+      setMessage('The word is in the dictionary!');
+    } else {
+      setMessage('The word is not in the dictionary.');
     }
   };
 
-  const handleSave = (word) => { // This function is called when the form is submitted
-    setSavedWords([...savedWords, word]); // The state is updated with the value of the input field
+  const handleSave = (word) => {
+    setSavedWords([...savedWords, word]);
   };
 
   return (
@@ -32,11 +36,11 @@ function App() {  // Component App is defined as a function that returns JSX cod
           This project has been built using React and Bootstrap.
         </p>
         <hr className="my-4" />
-        <Save onSave={handleSave} /> {/* The Save component is rendered */}
+        <Save onSave={handleSave} savedWords={savedWords} />
         <hr className="my-4" />
-        <form onSubmit={handleVerificationSubmit}> {/* The handleVerificationSubmit function is called when the form is submitted */}
+        <form onSubmit={handleVerificationSubmit}>
           <div className="form-group">
-            <label htmlFor="exampleFormControlInput2"> {/* The label is linked to the input field */}
+            <label htmlFor="exampleFormControlInput2">
               Verify if the word is in the dictionary
             </label>
             <input
@@ -48,12 +52,11 @@ function App() {  // Component App is defined as a function that returns JSX cod
               onChange={handleVerificationChange}
             />
           </div>
-          <ul></ul>
           <button type="submit" className="btn btn-primary">
             Verify
           </button>
         </form>
-        {message && (
+        {showMessage && ( // Only render the message if showMessage is true
           <div className={`alert ${savedWords.includes(verificationText) ? 'alert-success' : 'alert-danger'} mt-3`} role="alert">
             {message}
           </div>
